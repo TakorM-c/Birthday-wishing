cdc1d3a061e71382700eb01e9e9eacbf2d9d8e0d
TakorM-c <83616091+TakorM-c@users.noreply.github.com>
2021-11-06 16:32:48 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+birthday_cake_images_array=["download.jpg","happy-birthday-song-fact.jpg","R.jpg"];
+random_number=Math.floor(Math.random()*4);
+background_image=birthday_cake_images_array[random_number];
