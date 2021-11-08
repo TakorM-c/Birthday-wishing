a531a62e663aed96ba30a887d918d3ca33512bc1
TakorM-c <83616091+TakorM-c@users.noreply.github.com>
2021-11-08 17:15:22 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+canvas = document.getElementById('myCanvas');
+ctx=canvas.getContext("2d");
+
 birthday_cake_images_array=["download.jpg","happy-birthday-song-fact.jpg","R.jpg"];
 random_number=Math.floor(Math.random()*4);
 background_image=birthday_cake_images_array[random_number];
+function add(){
+    background_imgTag = new Image();//defining a variable with a new image 
+background_imgTag.onload = uploadBackground;//setting a function, onloading this variable 
+background_imgTag.src = background_image;// load image 
+
+}
+
+function uploadBackground(){
+ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
+}
